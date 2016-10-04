34a3ba7da7c307a60ab88f82f41f2b1e6504145e
xaf <xavier.mabras@protonmail.ch>
2016-10-04 10:19:46 +0200

    Initial Commit

---
+++ src/Ccc.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace CccValidate;
+
+/**
+ * Class Ccc.
+ *
+ * Description
+ *
+ * @link 
+ */
+class Ccc
+{
+    /**
+     * Ccc validation.
+     *
+     * @param mixed $entidad
+     * @param mixed $oficina
+     * @param mixed $numeroCuenta
+     * @param mixed $control
+     *
+     * @return bool
+     */
+    public static function validate($entidad, $oficina, $numeroCuenta, $control)
+    {
+        $dc = '';
+        $validations = array(6, 3, 7, 9, 10, 5, 8, 4, 2, 1);
+
+        foreach(array($entidad . $oficina, $numeroCuenta) as $cadena){
+            $suma = 0;
+
+            for($i = 0, $len = strlen($cadena); $i < $len; $i++){
+                $suma += $validations[$i] * substr($cadena, $len - $i - 1, 1);
+            }
+
+            $digito = 11 - $suma % 11;
+
+            if ($digito == 11) {
+                $digito = 0;
+            } elseif($digito == 10) {
+                $digito = 1;
+            }
+
+            $dc .= $digito;
+        }
+
+        return $control === $dc;
+    }
+
+}
---
+++ tests/CccTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use CccValidate\Ccc;
+
+/**
+ * Ccc validator test.
+ *
+ * @link 
+ *
+ * Pattern:
+ * EEEE OOOO DC CCCCCCCCCC
+ */
+class CccTest extends PHPUnit_Framework_TestCase
+{
+    public function testCorrectCcc()
+    {
+        $entidad = '2100';
+        $oficina = '3000';
+        $cuenta = '2103991515';
+        $dc = '12';
+
+        $this->assertEquals(true, Ccc::validate($entidad, $oficina, $cuenta, $dc));
+    }
+
+    public function testWrongCcc()
+    {
+        $entidad = '16350';
+        $oficina = '7823';
+        $cuenta = '7081002373';
+        $dc = '47';
+
+        $this->assertEquals(false, Ccc::validate($entidad, $oficina, $cuenta, $dc));
+    }
+}
